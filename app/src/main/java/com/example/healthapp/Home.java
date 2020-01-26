aee373d8e5cf10f6e60599fe3675d7abd2aa0b9f
Faraz Hafeez <21713096@bucks.ac.uk>
2020-01-26 21:11:40 +0000

    home page done need to design rest and finish up, (change/scale images as well)

--- app/src/main/java/com/example/healthapp/Home.java
+++ app/src/main/java/com/example/healthapp/Home.java
@@ -2,22 +2,35 @@ package com.example.healthapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ViewFlipper;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Home extends AppCompatActivity {
 
+    ViewPager vp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        vp = findViewById(R.id.SShow);
+        Imageadapter adapter = new Imageadapter(this);
+        vp.setAdapter(adapter);
 
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new slidetimer(), 2000, 4000);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.home);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -49,5 +62,26 @@ public class Home extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    public class slidetimer extends TimerTask{
+
+        @Override
+        public void run() {
+            Home.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(vp.getCurrentItem() == 0){
+                            vp.setCurrentItem(1);}
+                    else if(vp.getCurrentItem() == 1){
+                            vp.setCurrentItem(2);
+                    }
+                    else if(vp.getCurrentItem() == 2){
+                        vp.setCurrentItem(3);}
+                    else {
+                        vp.setCurrentItem(0);
+                    }
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/example/healthapp/Imageadapter.java
@@ -0,0 +1,44 @@
+package com.example.healthapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+
+
+public class Imageadapter extends PagerAdapter {
+    private Context mContext;
+    private int[] mImageIds = new int[]{R.drawable.note, R.drawable.health, R.drawable.three};
+
+    Imageadapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(mImageIds[position]);
+        container.addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ImageView) object);
+    }
+}
